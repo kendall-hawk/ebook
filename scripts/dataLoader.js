6af948a2cef9208b18a1ac7d89fbfa0a98649b68
kendall-hawk <58366663+kendall-hawk@users.noreply.github.com>
2025-06-07 00:34:02 +0800

    Update dataLoader.js

--- scripts/dataLoader.js
+++ scripts/dataLoader.js
@@ -1,1 +1,10 @@
+// dataLoader.js
+export async function loadTooltips() {
+  const res = await fetch('data/tooltips.json');
+  return await res.json();
+}
 
+export async function loadChapters() {
+  const res = await fetch('data/chapters.json');
+  return await res.json();
+}
