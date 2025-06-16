7bc4c12cde2234585c1c933fd0f8d02a6bb15b08
kendall-hawk <58366663+kendall-hawk@users.noreply.github.com>
2025-06-16 17:35:54 +0800

    更新 tokenizer.js

--- js/audio/tokenizer.js
+++ js/audio/tokenizer.js
@@ -1,1 +1,21 @@
+// js/audio/tokenizer.js
 
+/**
+ * 将句子分词为 { word, indexInText, length } 的数组
+ * 保留原始索引，便于匹配点击的单词位置。
+ */
+export function tokenizeText(sentence) {
+  const words = [];
+  const wordRegex = /\b\w+(?:['’]\w+)?\b/g;
+  let match;
+
+  while ((match = wordRegex.exec(sentence)) !== null) {
+    words.push({
+      word: match[0],
+      indexInText: match.index,
+      length: match[0].length,
+    });
+  }
+
+  return words;
+}
